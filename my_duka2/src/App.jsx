7be91e51114dfef000794b3670dde4a9e962058a
Osekobo <collinsboseko2005@gmail.com>
2026-03-05 23:16:19 +0300

    Tailwind

--- my_duka2/src/App.jsx
+++ my_duka2/src/App.jsx
@@ -1,35 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// import Header from "./assets/components/Header"
+// import Footer from "./assets/components/Footer"
 
-function App() {
+// const App = () => {
+//   const user = { name: "Jane", email: "jane@gmail.com" }
+//   const text = "A message from parent"
+//   return (
+//     <div>
+//       <p>This is a React app</p>
+//       <Header user={user} msg={text} />
+//       <div>
+//         <Footer x={user} y={text} />
+//       </div>
+//     </div>
+//   )
+// }
+
+// export default App
+
+import React, { useState } from 'react'
+const App = () => {
   const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  function increase(){
+    setCount{count +1}
+  }
 }
-
 export default App
---
+++ my_duka2/src/assets/pages/Products.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ProductsHero = () => {
+  return (
+    <div>
+      <h3 className='text-xl font-bold bg-red-500 m-4 p-4'>Welcome to the products page</h3>
+      <h5>See your products below</h5>
+    </div>
+  )
+}
+export default ProductsHero
